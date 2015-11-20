94bcea1484180e73a20de8b48f2c1f848f25f8fe
Rob Dimsdale <rdimsdale@pivotal.io>
2015-11-20 08:42:40 -0800

    Check deserializes input.
    - hard-coded knowlege of p-gitlab and p-mysql
    
    [#107305654]
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- cmd/check/main.go
+++ cmd/check/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+)
+
+type input struct {
+	Source struct {
+		APIToken     string `json:"api_token"`
+		ResourceName string `json:"resource_name"`
+	} `json:"source"`
+}
+
+type version []map[string]string
 
 func main() {
-	fmt.Println("{}")
+	var i input
+
+	err := json.NewDecoder(os.Stdin).Decode(&i)
+	if err != nil {
+		panic(err)
+	}
+
+	v := version{}
+	switch i.Source.ResourceName {
+	case "p-gitlab":
+		v = append(v, map[string]string{"version": "0.1.1 BETA"})
+	default:
+		v = append(v, map[string]string{"version": "1.7.1.0"})
+	}
+
+	err = json.NewEncoder(os.Stdout).Encode(v)
+	if err != nil {
+		panic(err)
+	}
 }
