6a70f88d6c399f51c44e47cfeca74684af8c20f8
Rob Dimsdale <rdimsdale@pivotal.io>
2015-11-30 09:00:42 -0800

    Pivnet client makes authenticated call to API.
    [#107305654]
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

--- pivnet_client.go
+++ pivnet_client.go
@@ -1,27 +1,59 @@
 package pivnet
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Response struct {
+	Releases []Release `json:"releases"`
+}
+
+type Release struct {
+	Version string `json:"version"`
+}
 
 type Client interface {
 	ProductVersions(string) ([]string, error)
 }
 
 type client struct {
-	url string
+	url   string
+	token string
 }
 
-func NewClient(url string) Client {
+func NewClient(url, token string) Client {
 	return &client{
-		url: url,
+		url:   url,
+		token: token,
 	}
 }
 
 func (c client) ProductVersions(id string) ([]string, error) {
-	productURL := c.url + "/products/" + id
-	fmt.Printf("productURL: %s\n", productURL)
-	_, err := http.Get(productURL)
-	return []string{"v0.0.0"}, err
+	releasesURL := c.url + "/products/" + id + "/releases"
+	req, err := http.NewRequest("GET", releasesURL, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Token: %s", c.token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	response := Response{}
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		panic(err)
+	}
+
+	var versions []string
+	for _, r := range response.Releases {
+		versions = append(versions, r.Version)
+	}
+
+	return versions, nil
 }
--- pivnet_client_test.go
+++ pivnet_client_test.go
@@ -21,32 +21,54 @@ var _ = Describe("PivnetClient", func() {
 	var (
 		server *ghttp.Server
 		client pivnet.Client
+		token  string
 	)
 
 	BeforeEach(func() {
 		server = ghttp.NewServer()
 		apiAddress := server.URL() + apiPrefix
-		client = pivnet.NewClient(apiAddress)
+		token = "my-auth-token"
+		client = pivnet.NewClient(apiAddress, token)
 	})
 
 	AfterEach(func() {
 		server.Close()
 	})
 
-	XIt("has authenticated headers for each request", func() {
+	It("has authenticated headers for each request", func() {
+		response := fmt.Sprintf(`{"releases": [{"version": "1234"}]}`)
+
+		server.AppendHandlers(
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", apiPrefix+"/products/my-product-id/releases"),
+				ghttp.VerifyHeaderKV("Authorization", fmt.Sprintf("Token: %s", token)),
+				ghttp.RespondWith(http.StatusOK, response),
+			),
+		)
+
+		_, err := client.ProductVersions("my-product-id")
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("gets versions", func() {
-		proudctVersion := "v" + strconv.Itoa(rand.Int())
+		productVersion := "v" + strconv.Itoa(rand.Int())
+		response := fmt.Sprintf(
+			`{"releases": [{"version": %q}, {"version": %q}]}`,
+			productVersion, productVersion,
+		)
+
 		server.AppendHandlers(
 			ghttp.CombineHandlers(
-				ghttp.VerifyRequest("GET", apiPrefix+"/products/my-product-id"),
-				ghttp.RespondWith(http.StatusOK, fmt.Sprintf(`{"products": [{"id": %s}]}`, productVersion)),
+				ghttp.VerifyRequest("GET", apiPrefix+"/products/my-product-id/releases"),
+				ghttp.RespondWith(http.StatusOK, response),
 			),
 		)
+
 		versions, err := client.ProductVersions("my-product-id")
 		Expect(err).NotTo(HaveOccurred())
 		Expect(server.ReceivedRequests()).Should(HaveLen(1))
+		Expect(versions).To(HaveLen(2))
 		Expect(versions[0]).Should(Equal(productVersion))
+		Expect(versions[1]).Should(Equal(productVersion))
 	})
 })
