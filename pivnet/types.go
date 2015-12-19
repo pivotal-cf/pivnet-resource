770e04572059d02a5fc3ab5422f53f3db8e3d94b
Rob Dimsdale <rdimsdale@pivotal.io>
2015-12-19 10:34:33 -0800

    Omit all empty pivnet types in json.
    [#110379392]

--- pivnet/types.go
+++ pivnet/types.go
@@ -1,38 +1,38 @@
 package pivnet
 
 type Response struct {
-	Releases []Release `json:"releases"`
+	Releases []Release `json:"releases,omitempty"`
 }
 
 type Release struct {
-	ID           int    `json:"id"`
-	Availability string `json:"availability"`
-	Eula         Eula   `json:"eula"`
-	OSSCompliant string `json:"oss_compliant"`
-	ReleaseDate  string `json:"release_date"`
-	ReleaseType  string `json:"release_type"`
-	Version      string `json:"version"`
-	Links        Links  `json:"_links"`
+	ID           int    `json:"id,omitempty"`
+	Availability string `json:"availability,omitempty"`
+	Eula         Eula   `json:"eula,omitempty"`
+	OSSCompliant string `json:"oss_compliant,omitempty"`
+	ReleaseDate  string `json:"release_date,omitempty"`
+	ReleaseType  string `json:"release_type,omitempty"`
+	Version      string `json:"version,omitempty"`
+	Links        Links  `json:"_links,omitempty"`
 }
 
 type Eula struct {
-	Slug    string `json:"slug"`
-	ID      int    `json:"id"`
-	Version string `json:"version"`
-	Links   Links  `json:"_links"`
+	Slug    string `json:"slug,omitempty"`
+	ID      int    `json:"id,omitempty"`
+	Version string `json:"version,omitempty"`
+	Links   Links  `json:"_links,omitempty"`
 }
 
 type ProductFiles struct {
-	ProductFiles []ProductFile `json:"product_files"`
+	ProductFiles []ProductFile `json:"product_files,omitempty"`
 }
 
 type ProductFile struct {
-	ID           int
-	AWSObjectKey string `json:"aws_object_key"`
-	Links        Links  `json:"_links"`
+	ID           int    `json:"id,omitempty"`
+	AWSObjectKey string `json:"aws_object_key,omitempty"`
+	Links        Links  `json:"_links,omitempty"`
 }
 
 type Links struct {
-	Download     map[string]string `json:"download"`
-	ProductFiles map[string]string `json:"product_files"`
+	Download     map[string]string `json:"download,omitempty"`
+	ProductFiles map[string]string `json:"product_files,omitempty"`
 }
