pivotal-cf/pivnet-resource

commit 9fab6e1a337b71c5e9134e7ffdbd99187c0ea5f8

Actually print to the screen instead of logFile
- Need to have separate sinks for pivnet client
- Needs easier to read logging for non-debug users

[#121470453]

check/check_command.go
@@ -2,8 +2,6 @@ package check
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/pivotal-cf-experimental/pivnet-resource/concourse"
@@ -15,7 +13,6 @@ import (
 
 type CheckCommand struct {
 	logger         lager.Logger
-	logFilePath    string
 	binaryVersion  string
 	filter         filter.Filter
 	pivnetClient   gp.Client
@@ -25,14 +22,12 @@ type CheckCommand struct {
 func NewCheckCommand(
 	binaryVersion string,
 	logger lager.Logger,
-	logFilePath string,
 	filter filter.Filter,
 	pivnetClient gp.Client,
 	extendedClient gp.ExtendedClient,
 ) *CheckCommand {
 	return &CheckCommand{
 		logger:         logger,
-		logFilePath:    logFilePath,
 		binaryVersion:  binaryVersion,
 		filter:         filter,
 		pivnetClient:   pivnetClient,
@@ -41,26 +36,6 @@ func NewCheckCommand(
 }
 
 func (c *CheckCommand) Run(input concourse.CheckRequest) (concourse.CheckResponse, error) {
-	logDir := filepath.Dir(c.logFilePath)
-	existingLogFiles, err := filepath.Glob(filepath.Join(logDir, "pivnet-resource-check.log*"))
-	if err != nil {
-		// This is untested because the only error returned by filepath.Glob is a
-		// malformed glob, and this glob is hard-coded to be correct.
-		return nil, err
-	}
-
-	for _, f := range existingLogFiles {
-		if filepath.Base(f) != filepath.Base(c.logFilePath) {
-			c.logger.Debug("Removing existing log file", lager.Data{"log_file": f})
-			err := os.Remove(f)
-			if err != nil {
-				// This is untested because it is too hard to force os.Remove to return
-				// an error.
-				return nil, err
-			}
-		}
-	}
-
 	c.logger.Debug("Received input", lager.Data{"input": input})
 
 	c.logger.Debug("Getting all valid release types")

cmd/check/main.go
@@ -33,22 +33,16 @@ func main() {
 
 	var input concourse.CheckRequest
 
-	logFile, err := ioutil.TempFile("", "pivnet-resource-check.log")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Fprintf(logFile, "PivNet Resource version: %s\n", version)
+	fmt.Fprintf(os.Stderr, "PivNet Resource version: %s\n", version)
 
-	fmt.Fprintf(os.Stderr, "Logging to %s\n", logFile.Name())
-
-	err = json.NewDecoder(os.Stdin).Decode(&input)
+	err := json.NewDecoder(os.Stdin).Decode(&input)
 	if err != nil {
-		fmt.Fprintf(logFile, "Exiting with error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Exiting with error: %v\n", err)
 		log.Fatalln(err)
 	}
 
 	sanitized := concourse.SanitizedSource(input.Source)
-	sanitizer := sanitizer.NewSanitizer(sanitized, logFile)
+	sanitizer := sanitizer.NewSanitizer(sanitized, os.Stderr)
 
 	l = lager.NewLogger("pivnet-resource")
 	l.RegisterSink(lager.NewWriterSink(sanitizer, lager.DEBUG))
@@ -66,7 +60,9 @@ func main() {
 		endpoint = pivnet.DefaultHost
 	}
 
-	ls := lagershim.NewLagerShim(l)
+	specialLogger := lager.NewLogger("pivnet client")
+	specialLogger.RegisterSink(lager.NewWriterSink(ioutil.Discard, lager.DEBUG))
+	sp := lagershim.NewLagerShim(specialLogger)
 
 	clientConfig := pivnet.ClientConfig{
 		Host:      endpoint,
@@ -75,17 +71,16 @@ func main() {
 	}
 	client := gp.NewClient(
 		clientConfig,
-		ls,
+		sp,
 	)
 
 	f := filter.NewFilter()
 
-	extendedClient := gp.NewExtendedClient(client, ls)
+	extendedClient := gp.NewExtendedClient(client, sp)
 
 	response, err := check.NewCheckCommand(
 		version,
 		l,
-		logFile.Name(),
 		f,
 		client,
 		extendedClient,

check/check_command_test.go
@@ -2,9 +2,6 @@ package check_test
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -23,9 +20,6 @@ var _ = Describe("Check", func() {
 		fakeFilter         *filterfakes.FakeFilter
 		fakePivnetClient   *gpfakes.FakeClient
 		fakeExtendedClient *gpfakes.FakeExtendedClient
-
-		tempDir     string
-		logFilePath string
 
 		testLogger lager.Logger
 
@@ -88,14 +82,6 @@ var _ = Describe("Check", func() {
 			},
 		}
 
-		var err error
-		tempDir, err = ioutil.TempDir("", "")
-		Expect(err).NotTo(HaveOccurred())
-
-		logFilePath = filepath.Join(tempDir, "pivnet-resource-check.log1234")
-		err = ioutil.WriteFile(logFilePath, []byte("initial log content"), os.ModePerm)
-		Expect(err).NotTo(HaveOccurred())
-
 	})
 
 	JustBeforeEach(func() {
@@ -117,16 +103,10 @@ var _ = Describe("Check", func() {
 		checkCommand = check.NewCheckCommand(
 			binaryVersion,
 			testLogger,
-			logFilePath,
 			fakeFilter,
 			fakePivnetClient,
 			fakeExtendedClient,
 		)
-	})
-
-	AfterEach(func() {
-		err := os.RemoveAll(tempDir)
-		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("returns the most recent version without error", func() {
@@ -152,39 +132,6 @@ var _ = Describe("Check", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(response).To(BeEmpty())
-		})
-	})
-
-	Context("when log files already exist", func() {
-		var (
-			otherFilePath1 string
-			otherFilePath2 string
-		)
-
-		BeforeEach(func() {
-			otherFilePath1 = filepath.Join(tempDir, "pivnet-resource-check.log1")
-			err := ioutil.WriteFile(otherFilePath1, []byte("initial log content"), os.ModePerm)
-			Expect(err).NotTo(HaveOccurred())
-
-			otherFilePath2 = filepath.Join(tempDir, "pivnet-resource-check.log2")
-			err = ioutil.WriteFile(otherFilePath2, []byte("initial log content"), os.ModePerm)
-			Expect(err).NotTo(HaveOccurred())
-		})
-
-		It("removes the other log files", func() {
-			_, err := checkCommand.Run(checkRequest)
-			Expect(err).NotTo(HaveOccurred())
-
-			_, err = os.Stat(otherFilePath1)
-			Expect(err).To(HaveOccurred())
-			Expect(os.IsNotExist(err)).To(BeTrue())
-
-			_, err = os.Stat(otherFilePath2)
-			Expect(err).To(HaveOccurred())
-			Expect(os.IsNotExist(err)).To(BeTrue())
-
-			_, err = os.Stat(logFilePath)
-			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 
@@ -236,7 +183,7 @@ var _ = Describe("Check", func() {
 				Expect(err).NotTo(HaveOccurred())
 
 				checkRequest.Version = concourse.Version{
-					versionWithETag,
+					ProductVersion: versionWithETag,
 				}
 			})
 
@@ -262,7 +209,7 @@ var _ = Describe("Check", func() {
 				Expect(err).NotTo(HaveOccurred())
 
 				checkRequest.Version = concourse.Version{
-					versionWithETag,
+					ProductVersion: versionWithETag,
 				}
 			})
 

cmd/in/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 
@@ -47,16 +46,10 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	logFile, err := ioutil.TempFile("", "pivnet-resource-in.log")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Fprintf(logFile, "PivNet Resource version: %s\n", version)
-
-	fmt.Fprintf(os.Stderr, "logging to %s\n", logFile.Name())
+	fmt.Fprintf(os.Stderr, "PivNet Resource version: %s\n", version)
 
 	sanitized := concourse.SanitizedSource(input.Source)
-	sanitizer := sanitizer.NewSanitizer(sanitized, logFile)
+	sanitizer := sanitizer.NewSanitizer(sanitized, os.Stderr)
 
 	l = lager.NewLogger("pivnet-resource")
 	l.RegisterSink(lager.NewWriterSink(sanitizer, lager.DEBUG))

cmd/out/main.go
@@ -60,16 +60,10 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	logFile, err := ioutil.TempFile("", "pivnet-resource-out.log")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Fprintf(logFile, "PivNet Resource version: %s\n", version)
-
-	fmt.Fprintf(os.Stderr, "logging to %s\n", logFile.Name())
+	fmt.Fprintf(os.Stderr, "PivNet Resource version: %s\n", version)
 
 	sanitized := concourse.SanitizedSource(input.Source)
-	sanitizer := sanitizer.NewSanitizer(sanitized, logFile)
+	sanitizer := sanitizer.NewSanitizer(sanitized, os.Stderr)
 
 	l := logger.NewLogger(sanitizer)
 
@@ -86,9 +80,11 @@ func main() {
 		UserAgent: useragent.UserAgent(version, "put", input.Source.ProductSlug),
 	}
 
+	specialLogger := logger.NewLogger(ioutil.Discard)
+
 	pivnetClient := pivnet.NewClient(
 		clientConfig,
-		l,
+		specialLogger,
 	)
 
 	bucket := input.Source.Bucket
@@ -108,7 +104,7 @@ func main() {
 		Bucket:          bucket,
 		Logger:          l,
 		Stdout:          os.Stdout,
-		Stderr:          logFile,
+		Stderr:          os.Stderr,
 		OutBinaryPath:   filepath.Join(outDir, s3OutBinaryName),
 	})
 

acceptance/check_test.go
@@ -72,6 +72,11 @@ var _ = Describe("Check", func() {
 		err := json.Unmarshal(session.Out.Contents(), &response)
 		Expect(err).ShouldNot(HaveOccurred())
 
+		By("Printing what is actually happening")
+		Eventually(session.Err).Should(gbytes.Say("pivnet-resource.Getting all valid release types"))
+		Eventually(session.Err).Should(gbytes.Say("pivnet-resource.Filtered versions"))
+		Eventually(session.Err).Should(gbytes.Say("pivnet-resource.Returning output"))
+
 		By("Validating all the expected elements were returned")
 		Expect(response).To(HaveLen(3))
 

acceptance/in_test.go
@@ -88,6 +88,10 @@ var _ = Describe("In", func() {
 		session := run(command, stdinContents)
 		Eventually(session, executableTimeout).Should(gexec.Exit(0))
 
+		By("Printing what is actually happening")
+		Eventually(session.Err).Should(gbytes.Say("Getting release"))
+		Eventually(session.Err).Should(gbytes.Say("Writing metadata to json file"))
+
 		By("Outputting a valid json response")
 		response := concourse.InResponse{}
 		err := json.Unmarshal(session.Out.Contents(), &response)

acceptance/out_test.go
@@ -140,6 +140,10 @@ var _ = Describe("Out", func() {
 			session := run(command, stdinContents)
 			Eventually(session, executableTimeout).Should(gexec.Exit(0))
 
+			By("Printing what is actually happening")
+			Eventually(session.Err).Should(gbytes.Say("metadata release parsed; contents:"))
+			Eventually(session.Err).Should(gbytes.Say("config used to create pivnet release"))
+
 			By("Validating new release exists on pivnet")
 			releases, err = pivnetClient.ReleasesForProductSlug(productSlug)
 			Expect(err).NotTo(HaveOccurred())

commit 597a762d5f6640959f12fdc14ade5b1a93b2c7d1

Check removes existing log files.
- fixes regression introduced in 9fab6e1a337b71c5e9134e7ffdbd99187c0ea5f8

[finishes #126472661]

check/check_command.go
@@ -3,6 +3,8 @@ package check
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/pivotal-cf-experimental/go-pivnet"
@@ -35,6 +37,7 @@ type CheckCommand struct {
 	filter        filter
 	pivnetClient  pivnetClient
 	semverSorter  sorter
+	logFilePath   string
 }
 
 func NewCheckCommand(
@@ -43,6 +46,7 @@ func NewCheckCommand(
 	filter filter,
 	pivnetClient pivnetClient,
 	semverSorter sorter,
+	logFilePath string,
 ) *CheckCommand {
 	return &CheckCommand{
 		logger:        logger,
@@ -50,11 +54,32 @@ func NewCheckCommand(
 		filter:        filter,
 		pivnetClient:  pivnetClient,
 		semverSorter:  semverSorter,
+		logFilePath:   logFilePath,
 	}
 }
 
 func (c *CheckCommand) Run(input concourse.CheckRequest) (concourse.CheckResponse, error) {
 	c.logger.Println("Received input, starting Check CMD run")
+
+	logDir := filepath.Dir(c.logFilePath)
+	existingLogFiles, err := filepath.Glob(filepath.Join(logDir, "pivnet-resource-check.log*"))
+	if err != nil {
+		// This is untested because the only error returned by filepath.Glob is a
+		// malformed glob, and this glob is hard-coded to be correct.
+		return nil, err
+	}
+
+	for _, f := range existingLogFiles {
+		if filepath.Base(f) != filepath.Base(c.logFilePath) {
+			c.logger.Printf("Removing existing log file: %s\n", f)
+			err := os.Remove(f)
+			if err != nil {
+				// This is untested because it is too hard to force os.Remove to return
+				// an error.
+				return nil, err
+			}
+		}
+	}
 
 	c.logger.Println("Getting all valid release types")
 	releaseTypes, err := c.pivnetClient.ReleaseTypes()

cmd/check/main.go
@@ -95,6 +95,7 @@ func main() {
 		f,
 		combinedClient,
 		s,
+		logFile.Name(),
 	).Run(input)
 	if err != nil {
 		log.Fatalf("Exiting with error: %s", err)

check/check_command_test.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/pivotal-cf-experimental/go-pivnet"
 	"github.com/pivotal-cf-experimental/pivnet-resource/check"
@@ -36,6 +38,9 @@ var _ = Describe("Check", func() {
 		releasesByVersionErr     error
 		etagErr                  error
 		logging                  *log.Logger
+
+		tempDir     string
+		logFilePath string
 	)
 
 	BeforeEach(func() {
@@ -83,6 +88,13 @@ var _ = Describe("Check", func() {
 			},
 		}
 
+		var err error
+		tempDir, err = ioutil.TempDir("", "")
+		Expect(err).NotTo(HaveOccurred())
+
+		logFilePath = filepath.Join(tempDir, "pivnet-resource-check.log1234")
+		err = ioutil.WriteFile(logFilePath, []byte("initial log content"), os.ModePerm)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	JustBeforeEach(func() {
@@ -105,7 +117,13 @@ var _ = Describe("Check", func() {
 			fakeFilter,
 			fakePivnetClient,
 			fakeSorter,
+			logFilePath,
 		)
+	})
+
+	AfterEach(func() {
+		err := os.RemoveAll(tempDir)
+		Expect(err).NotTo(HaveOccurred())
 	})
 
 	It("returns the most recent version without error", func() {
@@ -131,6 +149,39 @@ var _ = Describe("Check", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(response).To(BeEmpty())
+		})
+	})
+
+	Context("when log files already exist", func() {
+		var (
+			otherFilePath1 string
+			otherFilePath2 string
+		)
+
+		BeforeEach(func() {
+			otherFilePath1 = filepath.Join(tempDir, "pivnet-resource-check.log1")
+			err := ioutil.WriteFile(otherFilePath1, []byte("initial log content"), os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
+
+			otherFilePath2 = filepath.Join(tempDir, "pivnet-resource-check.log2")
+			err = ioutil.WriteFile(otherFilePath2, []byte("initial log content"), os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("removes the other log files", func() {
+			_, err := checkCommand.Run(checkRequest)
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = os.Stat(otherFilePath1)
+			Expect(err).To(HaveOccurred())
+			Expect(os.IsNotExist(err)).To(BeTrue())
+
+			_, err = os.Stat(otherFilePath2)
+			Expect(err).To(HaveOccurred())
+			Expect(os.IsNotExist(err)).To(BeTrue())
+
+			_, err = os.Stat(logFilePath)
+			Expect(err).NotTo(HaveOccurred())
 		})
 	})
 