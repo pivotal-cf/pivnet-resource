171f002e73154be467990b5e262895496a1d5786
Danny Sullivan <dsullivan@pivotal.io>
2017-04-18 17:53:37 -0400

    Add logging for file group creation
    [#142976615]

--- out/release/release_file_groups.go
+++ out/release/release_file_groups.go
@@ -4,6 +4,7 @@ import (
 	pivnet "github.com/pivotal-cf/go-pivnet"
 	"github.com/pivotal-cf/go-pivnet/logger"
 	"github.com/pivotal-cf/pivnet-resource/metadata"
+	"fmt"
 )
 
 type ReleaseFileGroupsAdder struct {
@@ -37,6 +38,11 @@ func (rf ReleaseFileGroupsAdder) AddReleaseFileGroups(release pivnet.Release) er
 	for _, fileGroup := range rf.metadata.FileGroups {
 		fileGroupID := fileGroup.ID
 		if fileGroupID == 0 {
+			rf.logger.Info(fmt.Sprintf(
+				"Creating file group with name: %s",
+				fileGroup.Name,
+			))
+
 			g, err := rf.pivnet.CreateFileGroup(pivnet.CreateFileGroupConfig{
 				ProductSlug: rf.productSlug,
 				Name: fileGroup.Name,
@@ -49,6 +55,10 @@ func (rf ReleaseFileGroupsAdder) AddReleaseFileGroups(release pivnet.Release) er
 			fileGroupID = g.ID
 		}
 
+		rf.logger.Info(fmt.Sprintf(
+			"Adding file group with ID: %d",
+			fileGroupID,
+		))
 		err := rf.pivnet.AddFileGroup(rf.productSlug, release.ID, fileGroup.ID)
 		if err != nil {
 			return err
