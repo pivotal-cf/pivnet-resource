23c0adb4376c5ceaec2fe167c039bf483dc79e1b
Paul Nikonowicz <pnikonowicz@pivotal.io>
2018-01-31 09:31:46 -0500

    add test to prevent error without endpoint provided
    [#154739895]
    
    Signed-off-by: Gabriel Dumitrescu <gdumitrescu@pivotal.io>

--- uaa/uaa_test.go
+++ uaa/uaa_test.go
@@ -2,11 +2,13 @@ package uaa_test
 
 import (
 	"errors"
+
+	"net/http"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 	"github.com/pivotal-cf/pivnet-resource/uaa"
-	"net/http"
 )
 
 var _ = Describe("UAA", func() {
@@ -27,7 +29,6 @@ var _ = Describe("UAA", func() {
 
 		It("returns a UAA token without error", func() {
 			response := uaa.AuthResp{Token: "some-uaa-token"}
-
 			server.AppendHandlers(
 				ghttp.CombineHandlers(
 					ghttp.VerifyRequest("POST", "/api/v2/authentication/access_tokens"),
@@ -42,7 +43,7 @@ var _ = Describe("UAA", func() {
 		})
 
 		Context("when UAA server responds with a non-200 status code", func() {
-			It("returns the error", func() {
+			It("returns the error 418", func() {
 				server.AppendHandlers(
 					ghttp.CombineHandlers(
 						ghttp.VerifyRequest("POST", "/api/v2/authentication/access_tokens"),
@@ -54,6 +55,20 @@ var _ = Describe("UAA", func() {
 				_, err := tokenFetcher.GetToken()
 				Expect(err).To(HaveOccurred())
 				Expect(err).To(MatchError(errors.New("failed to fetch API token - received status 418")))
+			})
+
+			It("returns an error without endpoint", func() {
+				tokenFetcher = uaa.NewTokenFetcher("", "some-refresh-token")
+				server.AppendHandlers(
+					ghttp.CombineHandlers(
+						ghttp.VerifyRequest("POST", "/api/v2/authentication/access_tokens"),
+						ghttp.VerifyBody([]byte(`{"refresh_token":"some-refresh-token"}`)),
+						ghttp.RespondWithJSONEncoded(http.StatusTeapot, nil),
+					),
+				)
+
+				_, err := tokenFetcher.GetToken()
+				Expect(err).To(HaveOccurred())
 			})
 		})
 
